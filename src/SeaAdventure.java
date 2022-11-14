ed2f52e25540d63ceb22b152d46badaea6741883
BT_4N2_11 <yhuang6817@bths.edu>
2022-11-14 12:31:33 -0500

    worked on project, the program skips the methods in the if statement

--- src/SeaAdventure.java
+++ src/SeaAdventure.java
@@ -1,10 +1,8 @@
 public class SeaAdventure {
 
-    private int ship;
     private int length;
     private int width;
     private int height;
-    private int size;
     private int count;
 
     public SeaAdventure (int length, int width, int height) {
@@ -12,19 +10,59 @@ public class SeaAdventure {
         this.width = width;
         this.height = height;
     }
+    public SeaAdventure (int length, int width) {
+        this.length = length;
+        this.width = width;
+        height = 250;
+    }
+    public SeaAdventure (int length) {
+        this.length = length;
+        width = 40;
+        height = 250;
+    }
     public SeaAdventure () {
-        ship = 320;
+        length = 250;
+        width = 25;
+        height = 150;
     }
 
     public boolean willSurviveTsunami () {
-        if (size >= 530 || ship >= 530) {
+        if (length >= 300 && width >= 30 && height >= 200) {
             return true;
         }
         return false;
     }
 
-    public String adventure () {
-
+    public String toString() {
+        return "Ship Length: " + length + "\nShip Width: " + width + "\nShip Height: " + height;
     }
+
+    public static boolean shipOnePw (String password) {
+        for (int i = 0; i <= password.length() / 2; i ++) {
+            char a = password.charAt(0);
+            char b = password.charAt(password.length() - 1);
+            if (a != b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean shipTwoPw (String password) {
+        String correct = "river";
+        if (password == correct) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean shipThreePw (String password) {
+        String correct = "human";
+        if (password == correct) {
+            return true;
+        }
+        return false;
+    }
+
 
 }
--- src/SeaAdventureRunner.java
+++ src/SeaAdventureRunner.java
@@ -6,9 +6,50 @@ public class SeaAdventureRunner {
         int length = (int)(Math.random() * 401) + 100;
         int width = (int)(Math.random() * 56) + 15;
         int height = (int)(Math.random() * 431) + 20;
+        System.out.print("Which ship would you like to choose (ship 1 to 4, with 1 the hardest to unlock and 4 the easiest)?");
+        int shipNum = s.nextInt();
+
         SeaAdventure Giant = new SeaAdventure(length, width, height);
+
+        if (shipNum == 1) {
+            System.out.println("Please think of a palindrome (a word that is spelled the same backwards) that is at least 6 letters: ");
+            String answer = s.nextLine();
+            if (SeaAdventure.shipOnePw(answer) == true) {
+                width = 40;
+                height = 250;
+                System.out.print("Great job, you have unlocked ship 1");
+            }
+        }
+        if (shipNum == 2) {
+            System.out.println("What is something that can run but cannot walk?");
+            String answer = s.nextLine();
+            if (SeaAdventure.shipTwoPw(answer) == true) {
+                height = 250;
+                System.out.print("Great job, you have unlocked ship 2");
+            }
+        }
+        if (shipNum == 3) {
+            System.out.println("What is this thing that walks on 4 legs in the morning, two legs in the afternoon, and 3 legs at night?");
+            String answer = s.nextLine();
+            if (SeaAdventure.shipThreePw(answer) == true) {
+                System.out.print("Great job, you have unlocked ship 3");
+            }
+        }
+        if (shipNum == 4) {
+            length = 250;
+            width = 25;
+            height = 150;
+            System.out.println("You chose the fourth ship...");
+        }
+
+        System.out.println(Giant.toString());
+
+
+
+
 
         
 
     }
+
 }
